f2a570d6464d1d8808e72693f5823b213d8d94b6
sergiomll1968 <sergiomaciaslluch@gmail.com>
2023-11-08 18:32:56 +0100

    Add new account creation functionality to Login page and create Register page with form and validation

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,19 +1,98 @@
-import { Container, Wrapper, Title, Form, Input, Button, Agreement } from './registerStyles.js';
+import { useState } from 'react';
+import REACT_APP_API_BASE_URL from '../../utils/constants.js';
+import { Container, Wrapper, Title, Form, Input, Button, Agreement, NotAgreement, ConditionsContainer, ButtonContainer } from './registerStyles.js';
+import { Modal } from '../../components/Modal/Modal.jsx';
+import { useFetchData } from '../../hooks/useFetchData.js';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [agreement, setAgreement] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { fetchData, data, error, loading } = useFetchData();
+  
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+    setTimeout(() => {
+      setIsModalOpen(false);
+    }, 3000);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    (!username || !email || !password || !confirmedPassword || !agreement) && handleOpenModal(true);
+    fetchData({
+      route: `${REACT_APP_API_BASE_URL}register`,
+      method: 'POST',
+      body: JSON.stringify({ 'username': username, 'email': email, 'password': password }),
+    });
+    
+  }
+
   return (
     <Container>
       <Wrapper>
         <Title>Crear una cuenta</Title>
         <Form>
-          <Input placeholder='username'/>
-          <Input placeholder='email'/>
-          <Input placeholder='password'/>
-          <Input placeholder='confirm password'/>
-          <Agreement>Al crear una cuenta, consiento que mis datos personales sean tratados de acuerdo a la <b>política de privacidad.</b></Agreement>
-          <Button>CREAR</Button>
+          <Input 
+            name='username'
+            placeholder='username'
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            name='email'
+            placeholder='email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            name='password'
+            placeholder='password'
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            name='confirmedPassword'
+            placeholder='confirm password'
+            type='password'
+            onChange={(e) => setConfirmedPassword(e.target.value)}
+          />
         </Form>
+        <ConditionsContainer>
+          <div>
+            <input
+              name='agreement'
+              type='checkbox'
+              onChange={(e) => setAgreement(e.target.checked)}
+            />
+          </div>
+          <div>
+            <div>
+              <Agreement>Al crear una cuenta, consiento que mis datos personales sean tratados de acuerdo a la <b>política de privacidad.</b></Agreement>
+            </div>
+            <div>
+              { !agreement && <NotAgreement>Debes aceptar la política de privacidad</NotAgreement>}
+            </div>
+          </div>
+        </ConditionsContainer>
+        <ButtonContainer>
+          <Button onClick={handleClick}>CREAR</Button>
+        </ButtonContainer>
       </Wrapper>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        showCloseButton={false}
+      >
+        <div>
+          Debes completar todos los campos y aceptar la política de privacidad
+        </div>
+      </Modal>
     </Container>
   )
 }
